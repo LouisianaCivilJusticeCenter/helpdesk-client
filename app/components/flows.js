6b88d088a5b371f14879b1f60a2f42ff0f2bfc6c
Shams Ali <ali.s90@outlook.com>
2016-12-08 11:17:05 -0600

    finished divorce flow chart

--- app/components/flows.js
+++ app/components/flows.js
@@ -1,22 +1,59 @@
+/* eslint max-len: 0 */
+
 const flows = {
   divorce: {
     title: 'this is title',
     question: {
       text: 'Have you or your spouse filed for a divorce?',
       yes: {
-        error: 'Due to Legal Ethics Concerns...call hotline',
+        error: 'Please call our hotline: (504) 355-0970',
       },
       no: {
         question: {
-          text: 'Do you have any minor children of the marriage?',
+          text: 'Are either you or your spouse represented by an attorney?',
           yes: {
+            error: 'Due to Legal Ethics Concerns, if you are represented by an attorney we cannot assist with this matter. If you spouse is represented, please call our Hotline: 504-355-0970',
+          },
+          no: {
             question: {
-              text: 'Have you been living seperate and apart for 365 days?',
+              text: 'Do you have any minor children of the marriage?',
               yes: {
                 question: {
-                  text: 'Do you have any community property that needs to be divided?',
+                  text: 'Have you been living seperate and apart for 365 days?',
                   yes: {
-                    success: 'success',
+                    question: {
+                      text: 'Do you have any community property that needs to be divided?',
+                      yes: {
+                        error: 'Please call our hotline: (504) 355-0970',
+                      },
+                      no: {
+                        question: {
+                          text: 'Is this a covenant marriage?',
+                          yes: {
+                            error: 'Please call our hotline: (504) 355-0970',
+                          },
+                          no: {
+                            question: {
+                              text: 'Are you or your spouse active members of the U.S. Armed Forces?',
+                              yes: {
+                                error: 'Please call our hotline: (504) 355-0970',
+                              },
+                              no: {
+                                question: {
+                                  text: 'Have you or your spouse lived in Louisiana for the last 6 months?',
+                                  yes: {
+                                    success: 'success',
+                                  },
+                                  no: {
+                                    error: 'Please call our hotline: (504 355-0970)',
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
                   },
                   no: {
                     error: 'Please call hotline',
@@ -24,18 +61,15 @@ const flows = {
                 },
               },
               no: {
-                error: 'Please call hotline',
-              },
-            },
-          },
-          no: {
-            question: {
-              text: 'Have you been living seperate and apart for 180 days?',
-              yes: {
-                success: 'success',
-              },
-              no: {
-                error: 'last',
+                question: {
+                  text: 'Have you been living seperate and apart for 180 days?',
+                  yes: {
+                    success: 'success',
+                  },
+                  no: {
+                    error: 'last',
+                  },
+                },
               },
             },
           },
